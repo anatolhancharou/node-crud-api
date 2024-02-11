e4a2157dd445d1e11a6a45bcde2534f2a4fbd5ca
Anatol Hancharou <anatolhancharou@gmail.com>
2024-02-12 00:55:41 +0300

    feat: implement user update

--- src/handlers/update-user.ts
+++ src/handlers/update-user.ts
@@ -1,11 +1,46 @@
 import { IncomingMessage, ServerResponse } from 'node:http';
+import { validate } from 'uuid';
+import { IUser } from '../types';
+import { getRequestUserData, isUserDataValid, setResponse } from '../helpers';
+import { StatusCode, ResponseMessage } from '../constants';
+import { users } from '../data-base';
 
 export const handleUserUpdate = async (
   req: IncomingMessage,
   res: ServerResponse,
   id: string | undefined,
 ): Promise<void> => {
-  console.log(req);
-  console.log(res);
-  console.log(id);
+  if (id && validate(id)) {
+    const userData: IUser | void = await getRequestUserData(req);
+
+    if (!userData || !isUserDataValid(userData)) {
+      return setResponse(
+        res,
+        StatusCode.BAD_REQUEST,
+        JSON.stringify({ message: ResponseMessage.BAD_REQUEST }),
+      );
+    }
+
+    const index = users.findIndex((user) => user.id === id);
+
+    if (index !== -1) {
+      const { username, age, hobbies } = userData;
+
+      users[index] = { ...users[index], username, age, hobbies } as IUser;
+
+      setResponse(res, StatusCode.OK, JSON.stringify(users[index]));
+    } else {
+      setResponse(
+        res,
+        StatusCode.NOT_FOUND,
+        JSON.stringify({ message: ResponseMessage.USER_NOT_FOUND }),
+      );
+    }
+  } else {
+    setResponse(
+      res,
+      StatusCode.BAD_REQUEST,
+      JSON.stringify({ message: ResponseMessage.BAD_REQUEST }),
+    );
+  }
 };
